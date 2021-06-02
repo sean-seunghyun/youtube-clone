f2a99d7371ed1491839e886879db00703a932626
sean-seunghyun <mmmpapi93@gmail.com>
2021-06-02 10:22:35 +0900

    controllers and routers

---
+++ src/controllers/userController.js
@@ -0,0 +1,16 @@
+
+export const edit = (req, res) => {
+    res.send("edit user");
+}
+
+export const remove = (req, res) => {
+    res.send("remove user");
+}
+
+export const join = (req, res) => {
+    res.send("join user");
+}
+
+export const login = (req, res) => {
+    res.send("logIn user");
+}
---
+++ src/controllers/videoController.js
@@ -0,0 +1,7 @@
+export const trending = (req, res) => {
+    res.send("homepage videos");
+}
+
+export const watch = (req, res) => res.send("watch videos");
+
+export const edit = (req, res) => res.send("edit videos");
