096abfad6c7c3924e6a743494c47c2d3605d5903
sean-seunghyun <mmmpapi93@gmail.com>
2021-06-08 09:32:21 +0900

    mixin, iteration in pug

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,4 +1,38 @@
-export const trending = (req, res) => res.render('home', {pageTitle: "home"} );
+const user = {
+    name: "sean",
+    loggedIn: false
+}
+
+
+export const trending = (req, res) => {
+    const videos = [
+        {
+        title: "#1",
+        author: "sean",
+        comments: 3,
+        createdAt: "20210608",
+        view: 55,
+        star: 3
+        },
+        {
+        title: "check out #2",
+        author: "John",
+        comments: 3,
+        createdAt: "20210608",
+        view: 56,
+        star: 3.4
+        },
+        {
+        title: "#3 videos",
+        author: "Tesla",
+        comments: 3,
+        createdAt: "20210608",
+        view: 56,
+        star: 2.2
+        }
+    ];
+    return res.render('home', { pageTitle: "home", videos, user } );
+}
 
 export const watch = (req, res) => res.render('watch', {pageTitle: "watch"} );
 
