23b5b52faeeedc1e8555a4d2b937ba25c55a018c
sean.seunghyun <sean-seunghyun@users.noreply.github.com>
2021-07-11 23:24:11 +0900

    github login

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,6 +1,6 @@
 import User from "../models/User";
 import bcrypt from "bcrypt";
-
+import fetch from "node-fetch";
 
 
 export const edit = (req, res) => {
@@ -80,4 +80,56 @@ export const startGithubLogin = (req, res) => {
     const finalUrl = `${baseUrl}?${params}`;
     console.log(finalUrl);
     res.redirect(finalUrl);
+}
+
+export const finishtGithubLogin = async (req, res) => {
+    const baseUrl = 'https://github.com/login/oauth/access_token';
+    const config = {
+        client_id: 'c3df5bb74f57219db0b6',
+        client_secret:'6239a9bf918063ecedaac67c06237ae87736bff1',
+        code: req.query.code
+    }
+    const params = new URLSearchParams(config).toString();
+
+    let finalUrl = `${baseUrl}?${params}`;
+
+    let tokenRequest = await (await fetch(finalUrl,{
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json;charset=utf-8',
+            'Accept': 'application/json'
+        }
+        })).json();
+
+    if("access_token" in tokenRequest){
+        const {access_token} = tokenRequest;
+
+        const apiUrl = 'https://api.github.com/user'
+
+        const userData = await (
+            await fetch(apiUrl,{
+            headers: {
+                'Authorization': `token ${access_token}`,
+            },
+        })).json();
+        console.log(userData);
+
+        const emailData = await (
+            await fetch(`${apiUrl}/emails`,{
+            headers: {
+                'Authorization': `token ${access_token}`,
+            },
+        })).json();
+        console.log(emailData);
+
+       const email = emailData.find((email) => email.primary === true && email.verified === true);
+       if(!email) res.redirect('/login');
+       console.log(email);
+
+
+    }else{
+        return res.redirect('/login')
+    }
+
+
 }
--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,11 +1,12 @@
 import express from "express";
-import { edit, remove, logout, see, startGithubLogin } from "../controllers/userController"
+import { edit, remove, logout, see, startGithubLogin, finishtGithubLogin } from "../controllers/userController"
 const userRouter = express.Router();
 
 userRouter.get("/edit", edit);
 userRouter.get("/delete", remove);
 userRouter.get("/logout", logout);
-userRouter.get("/start-github-login", startGithubLogin)
+userRouter.get("/github/start", startGithubLogin)
+userRouter.get("/github/finish", finishtGithubLogin)
 userRouter.get("/:id(\\d+/)", see);
 
 export default userRouter;
